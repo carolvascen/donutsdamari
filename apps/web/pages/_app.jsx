691f0adc5f3517ade6a874a8b3300d770561b0c6
carolvascen <carolvascen@gmail.com>
2026-03-31 11:32:03 -0300

    Update _app.jsx

--- apps/web/pages/_app.jsx
+++ apps/web/pages/_app.jsx
@@ -1,3 +1,12 @@
+import { useEffect } from 'react'
+
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    const link = document.createElement('link')
+    link.href = 'https://fonts.googleapis.com/css2?family=Fredoka:wght@400;600;700&family=Outfit:wght@300;400;600&display=swap'
+    link.rel = 'stylesheet'
+    document.head.appendChild(link)
+  }, [])
+
   return <Component {...pageProps} />
 }
